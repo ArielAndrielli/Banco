25cb906d9425416d08ccc38ef2c6b62a9f9588fc
Ariel Andrielli <arielandrielli100@gmail.com>
2022-01-21 08:54:16 -0300

    Criacao do repositorio

---
+++ ExemploBanco/HashCode.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using System.Security.Cryptography;
+
+namespace ExemploBanco
+{
+    class HashCode
+    {
+        public string PassHash(string data)
+        {
+            SHA1 sha = SHA1.Create();
+            byte[] hashdata = sha.ComputeHash(Encoding.Default.GetBytes(data));
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hashdata.Length; i++)
+            {
+                sb.Append(hashdata[i].ToString());
+            }
+            return sb.ToString();
+        }
+    }
+}
